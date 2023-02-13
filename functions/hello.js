1f4bc9c7d969b46bd94a829badc43e044046ed4e
Kshateesh <kshateesh007@gmail.com>
2023-02-13 17:55:59 +0530

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -6,8 +6,14 @@ export default function handler(req, res) {
     res
       .status(200)
       .json({ method: "GET", route: "id", params: req.params })
+  } else {
+          console.log("Call not supported");
+        res
+      .status(500)
+      .json({ error: "Bad Request" })
+    
   }
   
-        console.log("call not supported");
+
 
 }
