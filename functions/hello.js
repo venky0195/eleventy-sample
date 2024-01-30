146b2cd0f3164642c3f8e72b094ba0d5d0950236
Venkatesh G <46915517+venky0195@users.noreply.github.com>
2024-01-30 13:19:03 +0530

    log env variables in hello function

--- functions/hello.js
+++ functions/hello.js
@@ -2,7 +2,11 @@ import { helper } from "./helper";
 
 export default function handler(req, res) {
   console.log("Console log from function hello!!");
-    res
+  console.log("process.env.CFX_LOG_TARGET_FEATURE_TOGGLE_ENABLED->",process.env.CFX_LOG_TARGET_FEATURE_TOGGLE_ENABLED);
+  console.log("process.env.CFX_TELEMETRY_SERVICE_ENDPOINT->",process.env.CFX_TELEMETRY_SERVICE_ENDPOINT);
+  console.log("process.env.CFX_LOG_SOURCE_TOKEN->",process.env.CFX_LOG_SOURCE_TOKEN);
+
+  res
       .status(200)
       .json({
         query: req.query,
