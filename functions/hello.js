4553661accde02ee561398aaa233d5013c4b39ee
Venkatesh G <46915517+venky0195@users.noreply.github.com>
2024-01-31 12:55:58 +0530

    Update console logs

--- functions/hello.js
+++ functions/hello.js
@@ -2,9 +2,6 @@ import { helper } from "./helper";
 
 export default function handler(req, res) {
   console.log("Console log from function hello!!");
-  console.log("process.env.CFX_LOG_TARGET_FEATURE_TOGGLE_ENABLED->",process.env.CFX_LOG_TARGET_FEATURE_TOGGLE_ENABLED);
-  console.log("process.env.CFX_TELEMETRY_SERVICE_ENDPOINT->",process.env.CFX_TELEMETRY_SERVICE_ENDPOINT);
-  console.log("process.env.CFX_LOG_SOURCE_TOKEN->",process.env.CFX_LOG_SOURCE_TOKEN);
 
   res
       .status(200)
