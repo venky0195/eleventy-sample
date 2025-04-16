b0d45fadd82f6d3273d9c386ba67c707b14d472d
Venkatesh <venkatesh.gopinath@contentstack.com>
2025-04-16 15:27:42 +0530

    update edge

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -24,8 +24,8 @@ export default async function handler(request, context) {
         // newHeaders.delete("X-Ms-Request-Id");
         // newHeaders.delete("X-Ms-Version");
         // newHeaders.delete("X-Envoy-Upstream-Service-Time");
-        // newHeaders.delete("etag");
-        newHeaders.set("etag", '"18b07cb1e1f4b4cd50f94cf6c7b2fedb"');
+        newHeaders.delete("etag");
+        // newHeaders.set("etag", '"18b07cb1e1f4b4cd50f94cf6c7b2fedb"');
         console.log("🔧 Compressible response detected:", {
             url: request.url,
             contentType,
