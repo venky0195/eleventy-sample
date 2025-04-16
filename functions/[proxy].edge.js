a8c4b87d03d673448fed2a2f6d19e81589f40081
Venkatesh <venkatesh.gopinath@contentstack.com>
2025-04-16 15:47:18 +0530

    update edge

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -17,18 +17,6 @@ export default async function handler(request, context) {
     const newHeaders = new Headers(response.headers);
 
     if (isCompressible) {
-        const bodyBuffer = await response.arrayBuffer();
-        const bodyUint8 = new Uint8Array(bodyBuffer);
-
-        // Compute SHA-1 hash
-        const hashBuffer = await crypto.subtle.digest("SHA-1", bodyBuffer);
-        const hashHex = Array.from(new Uint8Array(hashBuffer))
-            .map(b => b.toString(16).padStart(2, "0"))
-            .join("");
-
-        const weakETag = `W/"${bodyUint8.length.toString(16)}-${hashHex}"`;
-
-        newHeaders.set("ETag", weakETag);
     }
 
     return new Response(response.body, {
