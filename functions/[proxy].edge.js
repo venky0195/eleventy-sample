d016d3b984452e962d4751ed719d602bea43c04c
Venkatesh <venkatesh.gopinath@contentstack.com>
2025-04-16 15:38:26 +0530

    update edge

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -17,19 +17,8 @@ export default async function handler(request, context) {
     const newHeaders = new Headers(response.headers);
 
     if (isCompressible) {
-        // newHeaders.delete("content-md5");
-        // newHeaders.delete("Last-Modified");
-        // newHeaders.delete("Access-Control-Expose-Headers");
-        // newHeaders.delete("access-control-allow-origin");
-        // newHeaders.delete("X-Ms-Request-Id");
-        // newHeaders.delete("X-Ms-Version");
-        // newHeaders.delete("X-Envoy-Upstream-Service-Time");
-        newHeaders.delete("etag");
-        // newHeaders.set("etag", '"18b07cb1e1f4b4cd50f94cf6c7b2fedb"');
-        console.log("🔧 Compressible response detected:", {
-            url: request.url,
-            contentType,
-        });
+        //create etag and set header here
+        newHeaders.set("ETag", createETag(response.body));
     }
 
     return new Response(response.body, {
@@ -38,3 +27,9 @@ export default async function handler(request, context) {
         headers: newHeaders,
     });
 }
+
+function createETag(body, encoding = "utf8") {
+    const etag = require("etag");
+    const buf = !Buffer.isBuffer(body) ? Buffer.from(body, encoding) : body;
+    return etag(buf, { weak: true });
+  }
