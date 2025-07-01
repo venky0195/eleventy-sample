027289cc497b2fba0c865ffad594e742f99cbbd8
Venkatesh G <46915517+venky0195@users.noreply.github.com>
2025-07-01 13:14:20 +0530

    update edge fn

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,19 +1,25 @@
 export default async function handler(request, context) {
-    //print the request headers loop through the headers and print the key and value
-    
-    let requestHeaders = Object.fromEntries(request.headers);
-    console.log("req headers",requestHeaders)
+    try {
+        //print the request headers loop through the headers and print the key and value
 
-    const response = await fetch(request);
-    console.log("🙋🏻 from edge fn")
-   
-    //print the response headers loop through the headers and print the key and value
-   let responseHeaders = Object.fromEntries(response.headers);
-    console.log("resp headers",responseHeaders)
+        let requestHeaders = Object.fromEntries(request.headers);
+        console.log("req headers", requestHeaders)
 
-    return new Response(response.body, {
-        status: response.status,
-        statusText: response.statusText,
-        headers: newHeaders,
-    });
+        const response = await fetch(request);
+        console.log("🙋🏻 from edge fn")
+
+        //print the response headers loop through the headers and print the key and value
+        let responseHeaders = Object.fromEntries(response.headers);
+        console.log("resp headers", responseHeaders)
+
+        return new Response(response.body, {
+            status: response.status,
+            statusText: response.statusText,
+            headers: response.headers,
+        });
+    } catch (error) {
+        console.log('🚀 ~ handler ~ error:', error)
+
+    }
+
 }
