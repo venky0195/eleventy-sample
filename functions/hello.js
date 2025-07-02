de48e0797e97cacb649818ec747187a2324a9897
Venkatesh G <46915517+venky0195@users.noreply.github.com>
2025-07-02 10:32:00 +0530

    error handling in cf

--- functions/hello.js
+++ functions/hello.js
@@ -1,10 +1,17 @@
 export default async function handler(req, res) {
-    let requestHeaders = Object.fromEntries(req.headers);
-        console.log("req headers from cloud fn-->", requestHeaders)
+    try {
+        let requestHeaders = Object.fromEntries(req.headers);
+        console.log("req headers from cloud fn-->", requestHeaders);
 
   
   res.status(200).json({
-    message: "helloworld!",
-    results
+    message: "helloworld!"
   });
+    } catch (error) {
+        res.status(200).json({
+            message: "something went wrong",
+            error
+        })
+    }
+    
 }
