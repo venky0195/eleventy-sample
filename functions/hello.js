fbc27ac7c8f295c981a83fdff5b4c5cff9079c81
Venkatesh G <46915517+venky0195@users.noreply.github.com>
2025-07-02 10:35:01 +0530

    update cloud fn

--- functions/hello.js
+++ functions/hello.js
@@ -1,17 +1,15 @@
 export default async function handler(req, res) {
     try {
-        let requestHeaders = Object.fromEntries(req.headers);
+        const requestHeaders = req.headers;
         console.log("req headers from cloud fn-->", requestHeaders);
 
-  
-  res.status(200).json({
-    message: "helloworld!"
-  });
-    } catch (error) {
         res.status(200).json({
-            message: "something went wrong",
-            error
-        })
+            message: "helloworld!"
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong",
+            error: JSON.stringify(error)
+        });
     }
-    
 }
