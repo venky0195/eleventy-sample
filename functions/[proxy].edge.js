63365313babb16b55c5d86c5090678d78e0e06d2
Venkatesh G <46915517+venky0195@users.noreply.github.com>
2025-07-02 10:26:35 +0530

    add cloud fn

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -3,14 +3,9 @@ export default async function handler(request, context) {
         //print the request headers loop through the headers and print the key and value
 
         let requestHeaders = Object.fromEntries(request.headers);
-        console.log("req headers", requestHeaders)
+        console.log("req headers from edge fn-->", requestHeaders)
 
         const response = await fetch(request);
-        console.log("🙋🏻 from edge fn")
-
-        //print the response headers loop through the headers and print the key and value
-        let responseHeaders = Object.fromEntries(response.headers);
-        console.log("resp headers", responseHeaders)
 
         return new Response(response.body, {
             status: response.status,
---
+++ functions/hello.js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+    let requestHeaders = Object.fromEntries(request.headers);
+        console.log("req headers from cloud fn-->", requestHeaders)
+
+  
+  res.status(200).json({
+    message: "helloworld!",
+    results
+  });
+}
